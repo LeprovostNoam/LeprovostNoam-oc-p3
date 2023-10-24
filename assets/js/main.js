96545650251a71e180c6b145c3d0306fff254ea0
OpenClassrooms <noam@leprovost.pro>
2023-10-24 18:34:21 +0200

    Ajout du chargement de la liste des travaux dans la galerie au chargement de la page, filtrage des travaux suivant le filtre sélectionné

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,22 @@
 const endPoint = "http://localhost:5678/api";
 
-
+// Fonction générique pour effectuer des requêtes AJAX
 function request(url, type, data, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open(type, url, true);
 
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
-        callback(null, JSON.parse(xhr.responseText));
+            // Appelle le callback avec les données JSON en cas de succès
+            callback(null, JSON.parse(xhr.responseText));
         } else {
-        callback("Erreur de requête : " + xhr.status, null);
+            // Appelle le callback avec une erreur en cas d'échec
+            callback("Erreur de requête : " + xhr.status, null);
         }
     };
 
     xhr.onerror = function() {
+        // Appelle le callback en cas d'erreur réseau
         callback("Erreur réseau", null);
     };
 
@@ -25,42 +28,21 @@ function request(url, type, data, callback) {
     }
 }
 
+// Récupère la liste des catégories depuis l'API
 function getCategories(callback) {
     request(endPoint + "/categories", "GET", null, callback);
 }
 
+// Récupère la liste des travaux depuis l'API
 function getWorks(callback) {
     request(endPoint + "/works", "GET", null, callback);
-}
-
-function createGalleryItem(work) {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    img.src = work.imageUrl;
-    img.alt = work.title;
-    const figcaption = document.createElement("figcaption");
-    figcaption.textContent = work.title;
-
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
-}
-
-function displayWorks(works) {
-    gallery.innerHTML = ""; 
-    works.forEach(createGalleryItem);
-}
-
-function filterWorksByCategory(works, categoryId) {
-    return works.filter(function(work) {
-        return work.categoryId === categoryId;
-    });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const gallery = document.querySelector(".gallery");
     const filtersContainer = document.querySelector(".filters");
 
+    // Récupère les catégories et crée des boutons de filtre pour chaque catégorie
     getCategories(function(error, categories) {
         if (error) {
             console.error("Erreur : " + error);
@@ -69,11 +51,70 @@ document.addEventListener("DOMContentLoaded", function() {
                 var button = document.createElement("button");
                 button.className = "filter-button";
                 button.textContent = category.name;
-                button.dataset.id = category.id;
+                button.dataset.categoryId = category.id;
                 filtersContainer.appendChild(button);
             });
         }
     });
+
+    // Crée un élément de galerie pour un travail
+    function createGalleryItem(work) {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }
+
+    // Affiche une liste de travaux dans la galerie
+    function displayWorks(works) {
+        gallery.innerHTML = "";
+        works.forEach(createGalleryItem);
+    }
+
+    // Filtrer les travaux par catégorie
+    function filterWorksByCategory(works, categoryId) {
+        return works.filter(function(work) {
+            return work.categoryId === categoryId;
+        });
+    }
+
+    filtersContainer.addEventListener("click", function(event) {
+        if (event.target.classList.contains("filter-button")) {
+            // Enlever la classe "active" de tous les boutons de filtre
+            const filterButtons = filtersContainer.querySelectorAll(".filter-button");
+            filterButtons.forEach(function(button) {
+                button.classList.remove("active");
+            });
+
+            // Ajouter la classe "active" uniquement au bouton cliqué
+            event.target.classList.add("active");
+
+            const categoryId = parseInt(event.target.dataset.categoryId);
+
+            if (!isNaN(categoryId)) {
+                const filteredWorks = filterWorksByCategory(allWorks, categoryId);
+                displayWorks(filteredWorks);
+            } else {
+                displayWorks(allWorks);
+            }
+        }
+    });
+
+    let allWorks = [];
+
+    // Récupère tous les travaux au chargement de la page
+    getWorks(function(error, works) {
+        if (error) {
+            console.error("Erreur lors de la récupération des travaux : " + error);
+        } else {
+            allWorks = works;
+            displayWorks(allWorks);
+        }
+    });
 });
-  
-  
